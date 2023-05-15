ae7a396779a001f48b164df496feaabe59b7591d
udeaghad <dozie.udeagha@gmail.com>
2023-05-15 22:42:31 +0100

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <Typography variant="h3" sx={{color: 'blue'}}>Hello World</Typography>
+      
+    </div>
+  );
+}
+
+export default App;

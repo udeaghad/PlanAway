207666f0a9bf1cd9723cd2ca197a4b6f815109bf
udeaghad <dozie.udeagha@gmail.com>
2023-06-01 16:03:48 +0100

    Styled the activity card

---
+++ src/components/Activities/Style.ts
@@ -0,0 +1,17 @@
+import { styled } from '@mui/material/styles';
+import { Box, Button } from '@mui/material';
+import theme from '../../theme/theme';
+
+export const StyledOptimizeButton = styled(Button)({
+  variant: 'contained',
+  backgroundColor: theme.palette.primary.variant,
+  color: "#000000",
+  borderColor: '#000000',
+  borderRadius: 99,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.main,    
+    boxShadow: '1px 1px 1px 1px #000000',
+  },
+  textTransform: 'none',
+  boxShadow: "1px 1px 1px 1px #000000"
+});

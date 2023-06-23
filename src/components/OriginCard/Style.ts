68590123617e5396c828994dac2ca83140d88610
udeaghad <dozie.udeagha@gmail.com>
2023-06-23 11:20:00 +0100

    Resolve merge error

--- src/components/OriginCard/Style.ts
+++ src/components/OriginCard/Style.ts
@@ -1,6 +1,5 @@
 import { styled} from '@mui/material/styles';
 import { Typography } from '@mui/material';
-import theme from '../../theme/theme';
 
 export const StyledLocationNameText = styled(Typography)({
     fontWeight: "bold",

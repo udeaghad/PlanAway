87370200fdd6325454b4f810dd1361965310cb00
udeaghad <dozie.udeagha@gmail.com>
2023-06-29 15:42:34 +0100

    Clean up code on MYTrips.ysx

--- src/pages/MyTrips/MyTrips.tsx
+++ src/pages/MyTrips/MyTrips.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useAppSelector } from '../../hooks/storeHooks'
 import SavedTripsList from '../../components/SavedTripsList/SavedTripsList';
 import DisplaySelectedSavedTrip from '../../components/DisplaySelectedSavedTrip/DisplaySelectedSavedTrip';
 import OriginCard from '../../components/OriginCard/OriginCard';
-import { Box, Divider } from '@mui/material';
-import { StyledOriginCard, StyledSavedTripsListCont } from './Style';
+import { Box, Divider, IconButton, Typography } from '@mui/material';
+import { StyledOriginCard, StyledSavedTripsListCont, StyledContainer, StyledMobileMap, StyledJumpCont, StyledActivityAndMapCont, StyledDesktopMap, StyledMap, StyledTopButton } from './Style';
+import MapForMobile from '../../components/MapSection/MapForMobile';
+import { GoogleMap, DirectionsRenderer, Marker } from '@react-google-maps/api';
+import JumpButtonMobile from '../../components/JumpButton/JumpButtonMobile';
+import MapSection from '../../components/MapSection/MapSection';
+import theme from '../../theme/theme';
+import UpgradeIcon from '@mui/icons-material/Upgrade';
 
 
 const MyTrips = () => {
   const {trips} = useAppSelector(state => state)
+
+  const [map, setMap] = useState<any>(null);
+  const [mapToDisplay, setMapToDisplay] = useState<any>(null)
   
 
   const [tripToOpen, setTripToOpen] = React.useState<any>(null)
@@ -18,6 +27,42 @@ const MyTrips = () => {
     setTripToOpen(selectedTrip)
   }
 
+  const DirectionsService = new window.google.maps.DirectionsService();
+
+  const calculateRoute = async(index:number) => {
+    if (!tripToOpen.places) return;
+    if (!tripToOpen.origin) return;
+
+    const {details} = tripToOpen.origin;
+    
+
+    const result = await DirectionsService.route({
+    origin: Number(details.lat) + ',' + Number(details.lng),
+    destination: Number(details.lat) + ',' + Number(details.lng),
+    travelMode: window.google.maps.TravelMode.DRIVING,
+    waypoints: tripToOpen.places[index].items?.map((place: any) => {
+      return {
+        location: Number(place.latitude) + ',' + Number(place.longitude),
+        stopover: true
+      }
+    }), 
+    
+    }, (res: any, status: any) => {
+      if (status === window.google.maps.DirectionsStatus.OK) {
+      
+        return res
+      } else {
+        console.error(`error fetching directions ${res}`);
+      }
+    })
+    setMapToDisplay(result)
+  }
+
+  const handleShowMap = (index: number) => {
+    calculateRoute(index)
+  }
+
+
   return (
     <div>
       <StyledSavedTripsListCont>
@@ -25,17 +70,62 @@ const MyTrips = () => {
       </StyledSavedTripsListCont>
 
       <Divider />
-      { tripToOpen && 
-      <Box>
-        <StyledOriginCard>          
-            <OriginCard {...tripToOpen.origin} />          
-        </StyledOriginCard>
-        
-        <div>
-          <DisplaySelectedSavedTrip {...tripToOpen} />
-        </div>
-      </Box>
       
+      { tripToOpen && 
+        <Box>
+          <StyledOriginCard>          
+              <OriginCard {...tripToOpen.origin} />          
+          </StyledOriginCard>
+          
+          <StyledContainer>
+            <StyledMobileMap sx={{width: "100%"}}>
+              <MapForMobile
+                origin={tripToOpen.origin}
+                GoogleMap={GoogleMap}
+                Marker={Marker}
+                DirectionsRenderer={DirectionsRenderer}
+                setMap={setMap}  
+                directions={mapToDisplay}
+                map={map}            
+                />
+          </StyledMobileMap> 
+
+          <StyledJumpCont id="top">
+            <JumpButtonMobile dailyGroups={tripToOpen.places} />
+          </StyledJumpCont>
+
+          <StyledActivityAndMapCont>
+            <div>
+              <DisplaySelectedSavedTrip tripToOpen={tripToOpen}  handleShowMap={handleShowMap}/>
+            </div>
+
+            <StyledDesktopMap>
+              <StyledMap>
+                <MapSection 
+                  origin={tripToOpen.origin}
+                  GoogleMap={GoogleMap}
+                  Marker={Marker}
+                  DirectionsRenderer={DirectionsRenderer}
+                  setMap={setMap}  
+                  directions={mapToDisplay}
+                  map={map}                  
+                />
+              </StyledMap>
+            </StyledDesktopMap>
+          </StyledActivityAndMapCont>
+
+          </StyledContainer>
+
+          <StyledTopButton>
+            <IconButton sx={{color: theme.palette.secondary.variant}} aria-label="top" href='#top'>
+              <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                <UpgradeIcon sx={{fontSize: "1.5rem"}} />
+                <Typography variant="caption" component="span">TOP</Typography>                                
+              </Box>
+            </IconButton>
+          </StyledTopButton>
+        </Box>
+        
       }
 
 
--- src/pages/MyTrips/Style.ts
+++ src/pages/MyTrips/Style.ts
@@ -3,12 +3,12 @@ import { Paper, Box } from '@mui/material';
 import theme from '../../theme/theme';
 
 
-export const StyledMobileMap = styled(Box)({
-  display: "block",
-  [theme.breakpoints.up('tablet')]: {
-    display: "none",
-  },
-})
+// export const StyledMobileMap = styled(Box)({
+//   display: "block",
+//   [theme.breakpoints.up('tablet')]: {
+//     display: "none",
+//   },
+// })
 
 export const StyledOriginCard = styled(Paper)({
   margin: "5%",
@@ -29,4 +29,61 @@ export const StyledSavedTripsListCont = styled(Box)({
   margin: "2%",
   // height: "40vh",
   // overflow: "scroll",
+})
+
+export const StyledContainer = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+})
+
+export const StyledMobileMap = styled(Box)({
+  display: "block",
+  [theme.breakpoints.up('tablet')]: {
+    display: "none",
+  },
+})
+
+export const StyledJumpCont = styled(Box)({
+  display: "flex", 
+  justifyContent: "space-between", 
+  alignItems: "flex-end", 
+  marginLeft: "5%",
+  width: "90%",
+  marginBottom: "1%",
+  [theme.breakpoints.up('laptop')]: {
+    width: "40%",
+    marginLeft: "8%"
+  }
+})
+
+export const StyledActivityAndMapCont = styled(Box)({
+  display: "block",
+  [theme.breakpoints.up('laptop')]: {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: "8%"
+  }
+})
+
+export const StyledDesktopMap = styled(Box)({
+  margin: "0 5%",
+  [theme.breakpoints.up('laptop')]: {
+    width: "55%"
+  }
+});
+
+export const StyledMap = styled(Box)({
+  display: "none",
+  [theme.breakpoints.up('tablet')]: {
+    display: "block",
+  },
+});
+
+export const StyledTopButton = styled(Box)({
+  display: "none",
+  [theme.breakpoints.only('tablet')]: {
+    display: "flex", 
+    justifyContent: "flex-end", 
+    alignItems: "center"
+  }
 })

44abb8dcb236b9bd94db71a13120b2016b1d8dbb
udeaghad <dozie.udeagha@gmail.com>
2023-07-04 19:16:01 +0100

    Re-arrange code for login forms

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,113 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { Box, Typography } from '@mui/material';
+
 import LocationMainBar from '../../components/LocationBar/LocationMainBar';
 import { StyledContainer, StyledLocationContainer, StyledNavLink } from './Style';
 import LoginMain from '../../components/LoginForm/LoginMain';
-import { Box, Typography } from '@mui/material';
+import { useAppDispatch, useAppSelector } from '../../hooks/storeHooks';
+import { login as postLoginData, loginActions } from '../../features/auths/Login/loginSlice';
+import { getAllTrips } from '../../features/SavedTrip/SavedTrip';
+import { userActions } from '../../features/auths/user/userSlice';
+import { msgAction } from '../../features/msgHandler/msgHandler';
 
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 
 
 const LoginPage = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const { login, user: {user} } = useAppSelector((state) => state);
+
+  const [openBackDrop, setOpenBackDrop] = useState(false);
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const [loginButtonDisabled, setLoginButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    
+    if (user){
+      dispatch(loginActions.resetLogin())
+      dispatch(getAllTrips({token: user.token}))
+    } 
+
+  }, [user, dispatch])
+
+
+
+  useEffect(() => {
+    if(login.isLoading){
+      setOpenBackDrop(true)
+    } else {
+      setOpenBackDrop(false)
+    }
+    if (login.data && login.data.status === 'success'){
+      
+      dispatch(userActions.setUser(login.data))     
+ 
+      dispatch(msgAction.getSuccessMsg("User signed in successfully!"))
+      navigate(-1)
+      return
+    }
+    if ( login.error) {
+      dispatch(msgAction.getErrorMsg("Wrong email or password!"))
+      dispatch(loginActions.resetLogin())
+      return
+    }
+  }, [login, dispatch, navigate])
+
+  const handleLoginOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    if (loginData.email && loginData.password) {
+      setLoginButtonDisabled(false);
+    } else {
+      setLoginButtonDisabled(true);
+    }
+
+    setLoginData({
+      ...loginData,
+      [event.target.id]: event.target.value
+    })
+
+
+  }
+
+  const handleClose = () => {
+    setLoginButtonDisabled(true)
+    setLoginData({
+      email: "",
+      password: ""
+    })
+    navigate(-1)
+  };
+
+  const handleLogin = () => {
+    const { email, password } = loginData
+
+    if(email && loginData){
+      dispatch(postLoginData({email, password}))
+
+      setLoginData({
+        email: "",
+        password: ""
+      })
+    }
+
+  }
+
+  const handleNavigateToSignUp = () => {
+      navigate("/SignUp")
+  }
+
+
   return (
     <div>
       <StyledLocationContainer>
@@ -21,7 +121,15 @@ const LoginPage = () => {
             </Typography>
           </Box>
 
-          <LoginMain />
+          <LoginMain 
+            handleLoginOnChange={handleLoginOnChange}
+            handleClose={handleClose}
+            loginButtonDisabled={loginButtonDisabled}
+            handleLogin={handleLogin}
+            handleNavigateToSignUp={handleNavigateToSignUp}
+            loginData={loginData}
+            openBackDrop={openBackDrop}          
+          />
 
           <StyledNavLink
           to="/"
